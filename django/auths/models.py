fccff74615d09e03da040a03c0f6a4767fb492f7
sandywonka <72706163+sandywonka@users.noreply.github.com>
2023-05-27 18:05:39 +0700

    Update models.py

--- django/auths/models.py
+++ django/auths/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+
+def validate_mobile_number(value):
+    if not value.startswith('+62'):
+        raise ValidationError('Mobile number must start with "+62".')
 
 class Registration(models.Model):
     GENDER_CHOICES = [
@@ -10,7 +17,7 @@ class Registration(models.Model):
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    mobile_number = models.CharField(max_length=20, unique=True)
+    mobile_number = models.CharField(max_length=20, unique=True, validators=[validate_mobile_number])
     date_of_birth = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
     email = models.EmailField(unique=True)
